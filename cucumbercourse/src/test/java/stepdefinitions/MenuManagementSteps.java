c118ab45f9b7679291fee5705704f181967f3845
mhpolin <mafruza.cse@gmail.com>
2023-08-31 11:24:28 +0200

    Some import changes and added files
    changed imports of AppTest.java, added MenuManagement feature file,added
    MenuManagementSteps file

---
+++ cucumbercourse/src/test/java/stepdefinitions/MenuManagementSteps.java
@@ -0,0 +1,27 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MenuManagementSteps {
+	
+	@Given("I have a menu item with name {string} with price {int}")
+	public void i_have_a_menu_item_with_name_with_price(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I add that menu item")
+	public void i_add_that_menu_item() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Menu item with the name {string} should be added")
+	public void menu_item_with_the_name_should_be_added(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
